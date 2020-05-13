6dfab0655b8215e8cd2675724c91c0aac3d5fa2e
Meena <rgmeena1404@gmail.com>
2020-05-13 12:24:49 +0200

    Updates

---
+++ precache-manifest.4d9c0268718cd2ca03c571555fe34a64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4dfeedde844d341794c67a6a7d4ac981",
+    "url": "/java-quiz/index.html"
+  },
+  {
+    "revision": "de0d8946f247a4298d1d",
+    "url": "/java-quiz/static/css/main.e596929b.chunk.css"
+  },
+  {
+    "revision": "6e751ab5d1d115688934",
+    "url": "/java-quiz/static/js/2.af8ed52e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/java-quiz/static/js/2.af8ed52e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de0d8946f247a4298d1d",
+    "url": "/java-quiz/static/js/main.13646bbd.chunk.js"
+  },
+  {
+    "revision": "af3d79dd60b27b52eb8f",
+    "url": "/java-quiz/static/js/runtime-main.0562669f.js"
+  }
+]);
